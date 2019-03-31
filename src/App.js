2e98e8f48f57ff649ea9acd44f05e2d247d9b056
hd2908 <im2908@naver.com>
2019-03-31 14:30:39 +0900

    initial project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Counter from 'components/Counter';
-import Buttons from 'components/Buttons';
-import Option from 'components/Options';
 
 class App extends React.Component {
     render(){
