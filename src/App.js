b8790a55c32c71a4408e91e6762a24e1d4d16428
hd2908 <im2908@naver.com>
2019-03-31 14:25:00 +0900

    initial project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Counter from 'components/Counter';
-import Buttons from 'components/Buttons';
-import Option from 'components/Options';
 
 class App extends React.Component {
     render(){
         return (
-            <div style={ {textAlign: 'center'} }>
-                <Counter/>
-                <Option/>
-                <Buttons/>
+            <div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import { combineReducers,createStore } from 'redux';
 import { Provider  } from 'react-redux';
 import App from 'App';
-import counter from 'modules/counterModule';
 
 const rootReducer = combineReducers({
-    counter
+
 });
 const store = createStore(rootReducer);
 const appElement = document.getElementById('root');
